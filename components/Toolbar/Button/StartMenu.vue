6e30186ee41f60c1db3938b1e500833fd6e35929
Victor Ribeiro <devjvribeiro@gmail.com>
2024-04-28 17:44:56 -0300

    fix: keep start menu the same active color on hover

--- components/Toolbar/Button/StartMenu.vue
+++ components/Toolbar/Button/StartMenu.vue
@@ -5,7 +5,9 @@
 <template>
   <ToolbarButton
     class="hover:text-green-300 active:text-green-500"
-    :class="[toolbar.startMenu.isOpen ? 'bg-white/20' : '']"
+    :class="[
+      toolbar.startMenu.isOpen ? 'bg-white/20 group-hover:bg-white/20' : '',
+    ]"
   >
     <Icon name="brands/windows-mono" class="text-[17px]" />
   </ToolbarButton>
