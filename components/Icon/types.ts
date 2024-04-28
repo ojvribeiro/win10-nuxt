2dcd99405019430ca3a0e98838a5e34c0496dddb
Victor Ribeiro <devjvribeiro@gmail.com>
2024-04-28 05:08:36 -0300

    feat: icons

---
+++ components/Icon/types.ts
@@ -0,0 +1,9 @@
+export interface IconSet {
+  name:
+    | 'task-view/idle'
+    | 'task-view/hover'
+    | 'brands/chrome'
+    | 'brands/vscode'
+    | 'brands/windows'
+    | 'brands/windows-mono'
+}
