9c0aff4f32860d9553be51bf48013ff7464a1899
Victor Ribeiro <devjvribeiro@gmail.com>
2024-05-03 02:41:17 -0300

    fix: prevent wallpaper image dragging

--- components/Desktop/Wallpaper.vue
+++ components/Desktop/Wallpaper.vue
@@ -8,7 +8,7 @@
   <div class="absolute left-0 top-0 z-[-1] h-screen w-screen bg-black">
     <NuxtImg
       :src="wallpaperModel"
-      class="h-full w-full object-cover object-center"
+      class="pointer-events-none h-full w-full object-cover object-center"
     />
   </div>
 </template>
