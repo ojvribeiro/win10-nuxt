a82261ae9a304a924f6bc9fd21955885e46756e1
Victor Ribeiro <devjvribeiro@gmail.com>
2024-05-06 00:56:20 -0300

    feat: ScrollAreaVertical component

--- components/ScrollArea/Vertical.vue
+++ components/ScrollArea/Vertical.vue
@@ -3,18 +3,32 @@
     class="h-full overflow-hidden transition-opacity duration-300"
     :scroll-hide-delay="0"
   >
-    <ScrollAreaViewport class="mr-4 h-full transition-opacity duration-300">
+    <ScrollAreaViewport class="mr-3 h-full transition-opacity duration-300">
       <slot />
     </ScrollAreaViewport>
 
-    <ScrollAreaScrollbar
-      orientation="vertical"
-      class="group w-3 hover:bg-[#3a3939]"
-    >
-      <ScrollAreaThumb
-        class="w-1 bg-[#a8a8a8] transition-[colors] [scale:0.1_1] hover:bg-[#888888] active:bg-[#b0b0b0] group-hover:[scale:1_1]"
-      />
-    </ScrollAreaScrollbar>
+    <Transition name="fade">
+      <ScrollAreaScrollbar
+        orientation="vertical"
+        class="group w-3 bg-transparent transition-colors delay-500 duration-300 hover:bg-[#3a3939]"
+      >
+        <ScrollAreaThumb
+          class="origin-right bg-[#888888] transition-[colors,_scale] delay-500 [scale:0.2_1] hover:!bg-[#888888] active:!bg-[#b0b0b0] group-hover:bg-[#616161] group-hover:transition-[colors,_scale] group-hover:delay-500 group-hover:duration-300 group-hover:[scale:1_1]"
+        />
+      </ScrollAreaScrollbar>
+    </Transition>
     <ScrollAreaCorner />
   </ScrollAreaRoot>
 </template>
+
+<style scoped>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.1s;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
