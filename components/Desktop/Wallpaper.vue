e9e34343375cdd3e793fffc29669d34e966e75ec
Victor Ribeiro <devjvribeiro@gmail.com>
2024-05-06 03:42:56 -0300

    temp: toggle wallpaper on click

--- components/Desktop/Wallpaper.vue
+++ components/Desktop/Wallpaper.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
-  const wallpaperModel = defineModel<string>({
-    default: '/img/wallpaper/windows10.jpg',
-  })
+  const settings = useSettingsStore()
+
+  function toggleWallpaper() {
+    if (
+      settings.personalization.background.picture.path ===
+      '/img/wallpaper/windows10.jpg'
+    ) {
+      changeWallpaper('/img/wallpaper/tetris.png')
+
+      return
+    }
+
+    changeWallpaper('/img/wallpaper/windows10.jpg')
+  }
+
+  function changeWallpaper(path: string) {
+    settings.personalization.background.picture.path = path
+  }
 </script>
 
 <template>
-  <div class="absolute left-0 top-0 z-[-1] h-screen w-screen bg-black">
+  <div
+    class="absolute left-0 top-0 z-[-1] h-screen w-screen bg-black"
+    @click="toggleWallpaper"
+  >
     <NuxtImg
-      :src="wallpaperModel"
+      :src="settings.personalization.background.picture.path"
       class="pointer-events-none h-full w-full object-cover object-center"
     />
   </div>
