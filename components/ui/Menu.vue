c4b482f0dced9f66feba54a731693970557a3203
Victor Ribeiro <devjvribeiro@gmail.com>
2025-05-19 05:15:23 -0300

    feat: Menu component

---
+++ components/ui/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <Transition name="menu">
+    <div
+      v-if="isOpen"
+      class="absolute bottom-0 left-0 h-[80%] bg-[#272626]/90 text-white shadow-[0px_4px_20px_-8px_#000] backdrop-blur-lg"
+    >
+      <slot />
+    </div>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+  const isOpen = defineModel<boolean>()
+</script>
+
+<style scoped>
+  @import '~/assets/css/animations/menu.css';
+</style>
