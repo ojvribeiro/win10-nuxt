793b217f3ae06e1bb8440df59b3ab34b56930679
Victor Ribeiro <devjvribeiro@gmail.com>
2025-05-21 06:05:16 -0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,23 @@
 // @ts-check
-import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  antfu({
-    rules: {
-      'curly': 'off',
-      'prettier/prettier': ['error'],
-      'style/arrow-parens': 'off',
-      'style/brace-style': 'off',
-      'style/quote-props': 'off',
-      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-      'vue/html-indent': ['error', 4],
-      'vue/operator-linebreak': 'off',
-      'vue/require-default-prop': 0,
-      'vue/singleline-html-element-content-newline': 0,
-    },
-  }),
-)
+export default withNuxt({
+  rules: {
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+
+    'vue/multi-word-component-names': 0,
+  },
+})
+
+
