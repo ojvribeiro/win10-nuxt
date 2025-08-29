28a60d14dda324bc0bdd331f7c6b6f32384b5e4b
Victor Ribeiro <devjvribeiro@gmail.com>
2025-08-29 11:48:04 -0300

    feat: Settings UWP component

---
+++ components/Apps/Uwp/Settings.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  const settings = useSettingsStore()
+
+  function toggleWallpaper() {
+    if (
+      settings.personalization.background.picture.path ===
+      '/img/wallpaper/windows10.jpg'
+    ) {
+      changeWallpaper('/img/wallpaper/tetris.png')
+
+      return
+    }
+
+    changeWallpaper('/img/wallpaper/windows10.jpg')
+  }
+
+  function changeWallpaper(path: string) {
+    settings.personalization.background.picture.path = path
+  }
+</script>
+
+<template>
+  <div>
+    <button @click="toggleWallpaper">Change wallpaper</button>
+  </div>
+</template>
